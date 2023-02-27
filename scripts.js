888fadfe383ea633fac1d2646f4dcc1fe4d56e01
Yasir <114235950+Yasir700@users.noreply.github.com>
2023-02-28 01:42:26 +0300

    Add task element buttons
    Remove button
    
    Done button

--- scripts.js
+++ scripts.js
@@ -9,6 +9,22 @@ let createInputPanel = () => {
     let descInput = createDescInput()
     let saveBtn = createSaveBtn()
 
+    saveBtn.addEventListener("click", () => {
+        let name = document.querySelector("#name-input").value
+        let desc = document.querySelector("#desc-input").value
+
+        if ((name == "" || desc == "")) {
+            showWarning("Can't be empty field")
+            return;
+        }
+
+
+        createTaskElement(id, name, desc)
+
+        document.querySelector("#name-input").value = ""
+        document.querySelector("#desc-input").value = ""
+    })
+
     document.body.append(inputPanel)
 
     function createNameInputLabel() {
@@ -58,15 +74,16 @@ let createShowPanel = () => {
     document.body.append(showPanel)
 }
 
+let id = 0
 let createTaskElement = (id, name, desc) => {
 
-    if (taskElements.length >= 8) {
-        console.log("You can't add any more task.")
+    if (taskElements.length >= 6) {
+        showWarning("You can't add any more task.")
         return
     }
 
     let taskElement = document.createElement("div")
-    taskElement.className = "task-element-" + id + " task-element"
+    taskElement.className = "task-element-" + id + " task-element" + " task-undone"
     showPanel.append(taskElement)
 
     let nameDiv = document.createElement("div")
@@ -79,12 +96,67 @@ let createTaskElement = (id, name, desc) => {
     descDiv.textContent = desc
     taskElement.append(descDiv)
 
+    let buttonDiv = document.createElement("div")
+    buttonDiv.className = "task-buttons-" + id + " task-buttons"
+    taskElement.append(buttonDiv)
+
+    let doneBtn = document.createElement("button")
+    doneBtn.addEventListener("click", () => {
+        if (taskElement.classList.contains("task-done")) {
+            doneBtn.textContent = "done"
+            taskElement.classList.remove("task-done")
+            taskElement.classList.add("task-undone")
+            removeBtn.disabled = true
+        } else {
+            doneBtn.textContent = "undone"
+            taskElement.classList.add("task-done")
+            taskElement.classList.remove("task-undone")
+            removeBtn.disabled = false
+        }
+    })
+    doneBtn.textContent = "done"
+    buttonDiv.append(doneBtn)
+
+    let removeBtn = document.createElement("button")
+    removeBtn.addEventListener("click", () => {
+        taskElement.remove()
+    })
+    removeBtn.textContent = "remove"
+    removeBtn.disabled = true
+    buttonDiv.append(removeBtn)
+
+
+    id++
     taskElements.push(taskElement)
 }
 
+let createWarningField = () => {
+    warningTxt = document.createElement("div")
+    warningTxt.className = "warning-field"
+    document.body.append(warningTxt)
+}
+
+let warningInterval = 3000
+let warningIsActive
+let showWarning = (message) => {
+    if (!warningIsActive) {
+        warningIsActive = true
+        warningTxt.textContent = message
+        setInterval(closeWarning, warningInterval)
+    }
+}
+let closeWarning = () => {
+    warningTxt.textContent = ""
+    warningIsActive = false
+    clearInterval(1)
+}
+
+
 let showPanel
 let inputPanel
+let warningTxt
 let taskElements = []
 
 createShowPanel();
+createWarningField()
 createInputPanel();
